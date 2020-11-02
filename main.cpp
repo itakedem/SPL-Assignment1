bab14bbf68b4eba953f85318c8e59936f1cd26c3
ItamarKedem <itakedem7683@gmail.com>
2020-11-02 15:34:24 +0200

    fixed the base

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-//
-// Created by Itamar Kedem on 02/11/2020.
-//
+#include <iostream>
+#include "Session.h"
 
-#include "main.h"
+using namespace std;
+
+int main(int argc, char** argv){
+    if(argc != 2)
+    {
+        cout << "usage cTrace <config_path>" << endl;
+        return 0;
+    }
+    Session sess(argv[1]);
+    sess.simulate();
+    return 0;
+}
