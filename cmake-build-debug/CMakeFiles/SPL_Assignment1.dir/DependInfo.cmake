987edc2113769b4ba8e34e312b71591471ed2615
ItamarKedem <itakedem7683@gmail.com>
2020-11-05 17:53:10 +0200

    wierd shit is going on

---
+++ cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/itakedem/CLionProjects/SPL-Assignment1/Agent.cpp" "/Users/itakedem/CLionProjects/SPL-Assignment1/cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/Agent.cpp.o"
+  "/Users/itakedem/CLionProjects/SPL-Assignment1/Graph.cpp" "/Users/itakedem/CLionProjects/SPL-Assignment1/cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/Graph.cpp.o"
+  "/Users/itakedem/CLionProjects/SPL-Assignment1/Session.cpp" "/Users/itakedem/CLionProjects/SPL-Assignment1/cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/Session.cpp.o"
+  "/Users/itakedem/CLionProjects/SPL-Assignment1/Tree.cpp" "/Users/itakedem/CLionProjects/SPL-Assignment1/cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/Tree.cpp.o"
+  "/Users/itakedem/CLionProjects/SPL-Assignment1/main.cpp" "/Users/itakedem/CLionProjects/SPL-Assignment1/cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL_Assignment1.dir/Agent.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/Graph.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/Session.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/Tree.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/main.cpp.o"
+  "SPL_Assignment1"
+  "SPL_Assignment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPL_Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
